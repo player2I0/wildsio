a0e8bcbef5706ee1f5acbb7835c72686b88b8399
player2I0 <59975613+player2I0@users.noreply.github.com>
2023-10-23 20:45:29 +0200

    Add files via upload

---
+++ env.js
@@ -0,0 +1,20 @@
+window.ENV = {};
+
+ENV.LOBBY_URL = "localhost";
+ENV.iog_host = "instantonline.io";
+ENV.forum_host = "forum.instantonline.io";
+
+ENV.useTempLogin = false;
+ENV.siteLocked = false;
+ENV.iaps = true;
+ENV.forum = true;
+ENV.ads = true;
+ENV.showFullscreen = true;
+
+/* new */
+
+ENV.wsPort = 8081;
+ENV.wssPort = 443;
+
+ENV.lobbyWsPort = 8080;
+ENV.lobbyWssPort = 443;
